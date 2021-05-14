0623909fcccfa6682e7e7e986cf1329d6ef6fa94
John Flournoy <john_flournoy@fas.harvard.edu>
2021-05-14 15:31:57 -0400

    holdout data

--- create_fitbit_variables.R
+++ create_fitbit_variables.R
@@ -1,11 +1,12 @@
 library(data.table)
 library(lubridate)
-data.table::setDTthreads(4)
+data.table::setDTthreads(1)
+
 aggregate_and_widen <- function(d){
   d[, fit_ss_date_posix := lubridate::mdy(fit_ss_date)]
   setorder(d, subjectkey, fit_ss_date_posix)
   d[, daynum := as.numeric(fit_ss_date_posix - fit_ss_date_posix[[1]]), by = 'subjectkey']
-  d <- d[daynum <= 21]
+  d <- d[daynum <= 14]
   d[, c('fit_ss_date', 'fit_ss_date_posix', 'fit_ss_protocol_wear', 'fit_ss_wkno') := NULL]
   d_day_l <- melt(d, id.vars = c(idvars, 'daynum'))
   d_l <- copy(d_day_l)
@@ -73,15 +74,40 @@ cbcl <- fread('~/data/ABCD/abcd_cbcls01_2d.txt',
               select = c(idvars, cbcl_vars))[eventname == '2_year_follow_up_y_arm_1']
 
 
+sprintf('%d subjects without fitbit activity data', cbcl[!fbdpas, on = 'subjectkey'][, .N])
+sprintf('%d subjects without fitbit sleep data', cbcl[!fbdss, on = 'subjectkey'][, .N])
+sprintf('%d subjects with fitbit activity but not sleep data', unique(fbdpas[!fbdss, on = 'subjectkey'][, 'subjectkey'])[, .N])
+
 setnames(fbdpas, 'fit_ss_wear_date', 'fit_ss_date')
 setnames(fbdss, 'fit_ss_sleepdate', 'fit_ss_date')
-fbdpas_w <- aggregate_and_widen(fbdpas)
-fbdss_w <- aggregate_and_widen(fbdss)
+fbdpas_w <- aggregate_and_widen(fbdpas)[, has_activity := 1]
+fbdss_w <- aggregate_and_widen(fbdss)[, has_sleep := 1]
+cbcl[, has_clinical := 1]
 cbcl[, clinical := fifelse(cbcl_scr_syn_internal_t > 64 |
                              cbcl_scr_syn_external_t > 64 |
                              cbcl_scr_syn_totprob_t > 64, 1, 0)]
 #cbcl[, sum(clinical == 1, na.rm = TRUE)/.N]
 
 data_wide <- merge(cbcl, merge(fbdpas_w, fbdss_w, by = idvars, all = TRUE), by = idvars, all = TRUE)
+head(names(data_wide))
+system.time({
+  print(
+    hist(
+      melt(data_wide)[, 
+                      list(prop_missing = round(sum(is.na(value))/.N, 2)), 
+                      by = 'variable'][, prop_missing],
+      breaks = 100,
+      xlab = 'Proportion missing values',
+      main = 'Missing data by variable'))})
+system.time({
+  print(
+    hist(
+      melt(data_wide, 
+           id.vars = 'subjectkey')[, 
+                                   list(prop_missing = round(sum(is.na(value))/.N, 2)), 
+                                   by = 'subjectkey'][, prop_missing], 
+      breaks = 100,
+      xlab = 'Proportion missing values',
+      main = 'Missing data by subject'))})
 
 fwrite(data_wide, file = 'cleaned_data.csv')
